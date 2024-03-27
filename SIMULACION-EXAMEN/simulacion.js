f5f4cd6d4f385c48eda376dfa571dc29e883083b
Alejandra Alaniz <dominguezale377@gmail.com>
2024-03-26 21:12:59 -0300

    Ejercicio mejorado

--- SIMULACION-EXAMEN/simulacion.js
+++ SIMULACION-EXAMEN/simulacion.js
@@ -8,20 +8,29 @@
 // Nota mayor que 6  - PROMOCIONADO.
 
 
-let nota = parseInt(prompt('Ingrese la nota obtenida:'));
+let nota1 = parseInt(prompt('Ingrese la primer nota obtenida:'));
+let nota2 = parseInt(prompt('Ingrese la segunda  nota obtenida:'));
+let nota3 = parseInt(prompt('Ingrese la tercer nota obtenida:'));
+let promedio = (nota1 + nota2 + nota3) / 3;
 
-function Califaciones(nota) {
+function Califaciones(nota1,nota2,nota3) {
     
-    if(isNaN(nota) || nota <= 0 || nota > 10){
+    if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) 
+    || nota1 <= 0 || nota1 > 10 
+    || nota2 <= 0 || nota2 > 10
+    || nota3 <= 0 || nota3 > 10){
         alert('Dato ingresado incorrecto ⚠️');
     }
-    else if(nota < 4){
-        alert(`Tu nota es ${nota} tu condición es: LIBRE.`)
-    }else if(nota >= 4 && nota < 6){
-        alert(`Tu nota es ${nota} tu condición es: REGULAR.`)
+    else if(promedio < 4 ){
+        alert(`Tu promedio es ${promedio.toFixed(0)}, tu condición es: LIBRE.`)
+        return promedio
+    }else if(promedio >= 4 && promedio < 6){
+        alert(`Tu promedio es ${promedio.toFixed(0)}, tu condición es: REGULAR.`)
+        return promedio
     }else {
-        alert(`Tu nota es ${nota} tu condición es: PROMOCIONADO.`)
+        alert(`Tu promedio es ${promedio.toFixed(0)}, tu condición es: PROMOCIONADO.`)
+        return promedio
     }
 }
-Califaciones(nota);
+Califaciones(nota1,nota2,nota3);
 
